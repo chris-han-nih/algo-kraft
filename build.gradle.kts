9d1b4359b2b397d0679c2ca9cc083086f86b9040
chris.han <namilhan@gmail.com>
2024-02-25 16:02:17 +0900

    Add Kotest dependencies to build.gradle.kts
    The updated build.gradle.kts now includes Kotest dependencies, specifically the junit5 runner, assertions core, and property modules. This change will enable us to use Kotest's testing features in our project.

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,9 @@ repositories {
 
 dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:5.8.0")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:5.8.0")
+    testImplementation("io.kotest:kotest-property-jvm:5.8.0")
 }
 
 tasks.test {
